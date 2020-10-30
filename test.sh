9ebef978ca8b8efd24445e1c62c8b9e6cf0bcc75
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-10-30 14:19:50 -0400

    Create Testing
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW It can run a Java program: $ANSI_RESET"
+docker run -it --rm -n tomcat -p 8888:8080 quay.io/ibmz/tomcat:10.0
+curl 0.0.0.0:8888
+docker stop tomcat
+docker network rm somenetwork
