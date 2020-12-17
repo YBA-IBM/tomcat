d257eec88c8a62fd9b104b65b0902455c440c0bc
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-12-16 19:27:39 -0500

    Update test.sh
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -10,6 +10,6 @@ echo -e "\n $ANSI_YELLOW *** FUNCTIONAL TEST(S) *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW Run basic functions described in readme: $ANSI_RESET"
 docker run -i -d --rm --name tomcat -p 8888:8080 quay.io/ibmz/tomcat:10.0.0
-curl 0.0.0.0:8888
+#curl 0.0.0.0:8888
 docker stop tomcat
 docker network rm somenetwork
