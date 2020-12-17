934d7867f7ee2bef0f15d8a5b21dbecb124568fa
YBA-IBM <47538942+YBA-IBM@users.noreply.github.com>
2020-12-16 19:30:18 -0500

    Remove uneeded line
    Signed-off-by: YBA-IBM <yoel.ben-avraham@ibm.com>

--- test.sh
+++ test.sh
@@ -12,4 +12,3 @@ echo -e "$ANSI_YELLOW Run basic functions described in readme: $ANSI_RESET"
 docker run -i -d --rm --name tomcat -p 8888:8080 quay.io/ibmz/tomcat:10.0.0
 #curl 0.0.0.0:8888
 docker stop tomcat
-docker network rm somenetwork
